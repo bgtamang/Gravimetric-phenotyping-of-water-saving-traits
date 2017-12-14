c14fcc29d3edbee563d65a8eafe4b48640d4ff0b
Bishal Gole Tamang <34286585+bishal-gole-tamang@users.noreply.github.com>
2017-12-14 15:58:30 -0600

    Update 3. R-Script: Extract model parameters.R

--- 3. R-Script: Extract model parameters.R
+++ 3. R-Script: Extract model parameters.R
@@ -4,36 +4,40 @@
 rm(list = ls())
 setwd()
 
-filelist <- list.files (pattern = ".csv")
+filelist <- list.files (pattern = ".txt")
 
 for (i in 1:length(filelist)) {
   
-  filename <-  read.csv (filelist[i], header = TRUE, sep = ",")
+  filename <-  read.table (filelist[i], header = TRUE, sep = "\t")
   filename$TR.Rate <- as.character (filename$TR.Rate)
-    if ((filename$TR.Rate[4] > 0.05) == TRUE) {
-      Model <- "Line"
-      Y.intercept <- filename$TR.Rate[33]
-      Y.intercept.SE <- filename$TR.Rate[36]
-      Slope.1 <- filename$TR.Rate[34]
-      Slope.1.SE <- filename$TR.Rate[37]
-      Slope.2 <- filename$TR.Rate[]
-      Slope.2.SE <- filename$TR.Rate[]
-      R.square <- filename$TR.Rate[43]
-    }
-    else if ((filename$TR.Rate[4] < 0.05) == TRUE) {
-      Model <- "Segmental"
-      Y.intercept <- filename$TR.Rate[11]
-      Y.intercept.SE <- filename$TR.Rate[16]
-      Slope.1 <- filename$TR.Rate[12]
-      Slope.1.SE <- filename$TR.Rate[17]
-      Slope.2 <- filename$TR.Rate[14]
-      Slope.2.SE <- filename$TR.Rate[19]
-      R.square <- filename$TR.Rate[27]
-    }
-    
- result <- data.frame (filelist[i], Model, Y.intercept, Y.intercept.SE,Slope.1,Slope.1.SE,Slope.2,Slope.2.SE, R.square)
- write.table(result, file= <path and file name>, row.names=FALSE,col.names=c("Genotype", "Model", "Y intercept", "Y intercept SE", "Slope 1", "Slope 1 SE", "Slope 2", "Slope 2 SE", "R square"), sep=",", append = TRUE)
- 
   
+  if (filename$TR.Rate[4] > 0.05) {
+    Model <- "Line"
+    Y.intercept <- filename$TR.Rate[33]
+    Y.intercept.SE <- filename$TR.Rate[36]
+    Slope.1 <- filename$TR.Rate[34]
+    Slope.1.SE <- filename$TR.Rate[37]
+    Slope.2 <- filename$TR.Rate[1]
+    BP <- filename$TR.Rate[1]
+    BP.SE <- filename$TR.Rate[1]
+    Slope.2.SE <- filename$TR.Rate[1]
+    R.square <- filename$TR.Rate[43]
+  }
+  else if (filename$TR.Rate[4] < 0.05) {
+    Model <- "Segmental"
+    Y.intercept <- filename$TR.Rate[11]
+    Y.intercept.SE <- filename$TR.Rate[16]
+    Slope.1 <- filename$TR.Rate[12]
+    Slope.1.SE <- filename$TR.Rate[17]
+    Slope.2 <- filename$TR.Rate[14]
+    BP <- filename$TR.Rate[13]
+    BP.SE <- filename$TR.Rate[18]
+    Slope.2.SE <- filename$TR.Rate[19]
+    R.square <- filename$TR.Rate[27]
+  }
+  
+  result <- data.frame (filelist[i], Model, Y.intercept, Y.intercept.SE,Slope.1,Slope.1.SE,Slope.2,Slope.2.SE, R.square, BP, BP.SE, row.names = FALSE)
+  write.table (result, file = "Model.comparison.LSq.csv", row.names= FALSE, col.names=FALSE, sep=",", append = TRUE)
+
 }
 
