3b573fd7bdaf05bb43cbbcc5d363b72f9fbab293
Bishal Gole Tamang <34286585+bishal-gole-tamang@users.noreply.github.com>
2017-12-29 15:36:14 -0600

    Update 3. R-Script: Extract model parameters.R

--- 3. R-Script: Extract model parameters.R
+++ 3. R-Script: Extract model parameters.R
@@ -1,43 +1,104 @@
 
+
 # Extracting parameters from model fitted in prism-generated files.
+
+
+# METHOD 1
+#####################################################################
+#                      Least Square Method                          #
+#####################################################################
+
 
 rm(list = ls())
 setwd()
 
-filelist <- list.files (pattern = ".txt")
+filelist <- list.files (pattern = ".csv")
 
 for (i in 1:length(filelist)) {
   
-  filename <-  read.table (filelist[i], header = TRUE, sep = "\t")
-  filename$TR.Rate <- as.character (filename$TR.Rate)
+  filename <-  read.csv (filelist[i], header = TRUE, sep = ",")
+  filename$TR <- as.character (filename$TR)
   
-  if (filename$TR.Rate[4] > 0.05) {
+  if (filename$TR[4] > 0.05) {
     Model <- "Line"
-    Y.intercept <- filename$TR.Rate[33]
-    Y.intercept.SE <- filename$TR.Rate[36]
-    Slope.1 <- filename$TR.Rate[34]
-    Slope.1.SE <- filename$TR.Rate[37]
-    Slope.2 <- filename$TR.Rate[1]
-    BP <- filename$TR.Rate[1]
-    BP.SE <- filename$TR.Rate[1]
-    Slope.2.SE <- filename$TR.Rate[1]
-    R.square <- filename$TR.Rate[43]
-  }
-  else if (filename$TR.Rate[4] < 0.05) {
-    Model <- "Segmental"
-    Y.intercept <- filename$TR.Rate[11]
-    Y.intercept.SE <- filename$TR.Rate[16]
-    Slope.1 <- filename$TR.Rate[12]
-    Slope.1.SE <- filename$TR.Rate[17]
-    Slope.2 <- filename$TR.Rate[14]
-    BP <- filename$TR.Rate[13]
-    BP.SE <- filename$TR.Rate[18]
-    Slope.2.SE <- filename$TR.Rate[19]
-    R.square <- filename$TR.Rate[27]
+    Y.intercept <- filename$TR[33]
+    Y.intercept.SE <- filename$TR[36]
+    Slope.1 <- filename$TR[34]
+    Slope.1.SE <- filename$TR[37]
+    Slope.2 <- filename$TR[1]
+    Slope.2.SE <- filename$TR[1]
+    BP <- filename$TR[1]
+    BP.SE <- filename$TR[1]
+    p.value <-  filename$TR[4]
+    R.square <- filename$TR[43]
   }
   
-  result <- data.frame (filelist[i], Model, Y.intercept, Y.intercept.SE,Slope.1,Slope.1.SE,Slope.2,Slope.2.SE, R.square, BP, BP.SE, row.names = FALSE)
-  write.table (result, file = "Model.comparison.LSq.csv", row.names= FALSE, col.names=FALSE, sep=",", append = TRUE)
+  else if (filename$TR[4] < 0.05) {
+    Model <- "Segmental"
+    Y.intercept <- filename$TR[11]
+    Y.intercept.SE <- filename$TR[16]
+    Slope.1 <- filename$TR[12]
+    Slope.1.SE <- filename$TR[17]
+    Slope.2 <- filename$TR[14]
+    Slope.2.SE <- filename$TR[19]
+    BP <- filename$TR[13]
+    BP.SE <- filename$TR[18]
+    p.value <-  filename$TR[4]
+    R.square <- filename$TR[27]
+  }
+  
+  result <- data.frame (filelist[i], Model, Y.intercept, Y.intercept.SE,Slope.1,Slope.1.SE,Slope.2,Slope.2.SE, R.square, BP, BP.SE, p.value, row.names = FALSE)
+  write.table (result, file = "Model.parameter.extract.LSS.csv", row.names= FALSE, col.names=FALSE, sep=",", append = TRUE)
+  
+}
 
+
+# METHOD 2
+#####################################################################
+#              Akaike Information Criteria Method                   #
+#####################################################################
+
+
+rm(list = ls())
+setwd()
+
+filelist <- list.files (pattern = ".csv")
+
+for (i in 1:length(filelist)) {
+  
+  filename <-  read.csv (filelist[i], header = TRUE, sep = ",")
+  filename$TR <- as.character (filename$TR)
+  
+  if (filename$TR[3] > filename$TR[5]) {
+    Model <- "Line"
+    Y.intercept <- filename$TR[34]
+    Y.intercept.SE <- filename$TR[37]
+    Slope.1 <- filename$TR[35]
+    Slope.1.SE <- filename$TR[38]
+    Slope.2 <- filename$TR[1]
+    Slope.2.SE <- filename$TR[1]
+    BP <- filename$TR[1]
+    BP.SE <- filename$TR[1]
+    probability <-  filename$TR[3]
+    R.square <- filename$TR[44]
+  }
+  
+  else if (filename$TR[3] < filename$TR[5]) {
+    Model <- "Segmental"
+    Y.intercept <- filename$TR[12]
+    Y.intercept.SE <- filename$TR[17]
+    Slope.1 <- filename$TR[15]
+    Slope.1.SE <- filename$TR[20]
+    Slope.2 <- filename$TR[14]
+    Slope.2.SE <- filename$TR[19]
+    BP <- filename$TR[14]
+    BP.SE <- filename$TR[19]
+    probability <-  filename$TR[5]
+    R.square <- filename$TR[28]
+  }
+  
+  result <- data.frame (filelist[i], Model, Y.intercept, Y.intercept.SE,Slope.1,Slope.1.SE,Slope.2,Slope.2.SE, R.square, BP, BP.SE, probability, row.names = FALSE)
+  write.table (result, file = "Model.parameter.extract.AICc.csv", row.names= FALSE, col.names=FALSE, sep=",", append = TRUE)
+  
 }
 
